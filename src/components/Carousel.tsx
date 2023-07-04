6e72e14933b6fed9cc2ec2a19fd9f6fba805b4aa
moong <moonki0623@naver.com>
2023-07-04 21:32:15 +0900

    [feat]: Carousel Implemented

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import styled from "styled-components";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -32,21 +33,27 @@ const Carousel = () => {
       >
         <SwiperSlide>
           <div className="slide-wrapper">
-            <img
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
-              alt="img_src_temp"
-            />
+            <CarouselImgDiv imgSrc="https://swiperjs.com/demos/images/nature-1.jpg">
+              asdf
+            </CarouselImgDiv>
             {/* 
             TODO : ["] 전용 div 하나 생성하기
             TODO : main_text sub_text css styling
             */}
             <div className="swiper-slide-div">
-              <div className="swiper-slide-div-main_text">
-                휴대하기 좋은
-                <br /> 비건 세럼 립 틴트로
-                <br /> 어디서든 촉촉한 입술을
-                <br />
-                가꿔보세요. ”
+              <div className="swiper-slide-div-main_div">
+                “
+                <div className="swiper-slide-div-main_text">
+                  휴대하기 좋은
+                  <br /> 비건 세럼 립 틴트로
+                  <br /> 어디서든 촉촉한 입술을
+                  <br />
+                  가꿔보세요. ”
+                  <div className="swiper-slide-div-sub_text">
+                    Get moist lips with the portable
+                    <br /> vegan serum lip tint.
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -129,3 +136,17 @@ const Carousel = () => {
 };
 
 export default Carousel;
+
+const CarouselImgDiv = styled.div<{ imgSrc: string }>`
+  background-image: url(${(props) => props.imgSrc});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 20px;
+  display: flex;
+  min-width: 23.4vw;
+  min-height: 23.4vw;
+  box-shadow: 0px 4px 30px 0px rgba(158, 158, 158, 0.4);
+  z-index: 999;
+  position: relative;
+`;
