a0f4cd6de3983db0e94392db7e60d79a2e3f0e18
moong <moonki0623@naver.com>
2023-07-07 03:07:59 +0900

    [feat]: modal 기능 구현~!!!!!!!!!!!!!!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import HookingRouter from "@/router";
+import { loginModalOverlay } from "@/utils/atom";
+import { useRecoilValue } from "recoil";
 
 const App = () => {
+  const mounted = useRecoilValue(loginModalOverlay);
+
   return (
     <main className="app">
+      {mounted && <div className="hovered" />}
       <HookingRouter />
     </main>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import { theme } from "@/styles/theme";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
