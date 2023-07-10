cf6954d658184f1dd304f8167b295d46944b2a51
hyosin <>
2023-07-10 22:54:21 +0900

    test: 브랜드 카피 문구 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,58 +3,61 @@ import Header from "@/components/Header";
 import BrandCard from "@/components/BrandCard";
 import Filter from "@/components/Filter";
 import { Z_INDEX_MODAL } from "@/utils/constants";
+import { useEffect } from "react";
 
 import { useRecoilValue } from "recoil";
 import { modalOverlay } from "@/utils/atom";
 import styled from "styled-components";
 import IMG_BRAND_SAMPLE from "@/assets/images/icon-brand-sample.svg";
 import Carousel from "@/components/Carousel";
+import { getAllCopy } from "@/api/post";
+
 const CARD_DATA = [
   {
     idx: 0,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: ".찬바람 부는 날, 피부를 탄탄하게!\n\n\n#로열허니프로폴리스인리치배리어크림.\n\n\n블랙비 프로폴리스와 세라-그린콤플렉스가 만나 외부자극에 손상된 피부 장벽을 개선해주고 피부 장벽을 튼튼하게 만들어준답니다.💪🏼☺️진한 텍스처로 피부 보습까지 완벽하게 채워보세요!.#RoyalHoneyPropolisNourish your skin with Skin Food’s extraordinary Propolis lines.#스킨푸드 #탄탄장벽 #꿀찬광채 #꿀찬피부 #프로폴리스 #프로폴리스에센스 #프로폴리스크림 #크림추천",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 1,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 2,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 3,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 4,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 5,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 6,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum 휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
   {
     idx: 7,
-    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서든 촉촉한 입술을 가꿔보세요 Get moist lips with the portable vegan serum... 더 보기",
+    text: "휴대하기 좋은 비건 세럼 립틴트로 어디서",
     brand: "이니스프리",
     img: IMG_BRAND_SAMPLE,
   },
@@ -83,11 +86,22 @@ const CARD_DATA = [
     img: IMG_BRAND_SAMPLE,
   },
 ];
+
 const Home = () => {
   const searchFocus = useRecoilValue(modalOverlay);
+
+  const getCopyData = async () => {
+    //const data = await getAllCopy();
+    //if (data) {
+    // }
+  };
+
+  useEffect(() => {
+    getCopyData();
+  }, []);
+
   return (
     <>
-      <Header />
       {searchFocus && <Overlay />}
       <CarouselDiv>
         <Carousel />
@@ -137,6 +151,6 @@ const Overlay = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: black;
-  opacity: 0.5;
+  opacity: 0.6;
   z-index: ${Z_INDEX_MODAL};
 `;
