9a05505180130ffba471c6bac457ac04676cac3b
moong <moonki0623@naver.com>
2023-07-16 19:28:01 +0900

    feat: brand specific page updated

--- src/components/BrandBanner.tsx
+++ src/components/BrandBanner.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import imgData from "@/assets/datas/imgData.json";
+import BrandIcon from "@/components/BrandIcon";
+import BrandMoodButton from "@/components/BrandMoodButton";
 
 interface BrandBannerProps {
   name: string;
@@ -14,6 +16,25 @@ const BrandBanner = ({ name }: BrandBannerProps) => {
         src={require(`../assets/images/brandBanner/${targetData.name_kr}.png`)}
         alt="brandBanner"
       />
+      <BrandBannerInsideDiv>
+        <div className="brandDescSection">
+          <div className="brandDescTop">
+            <BrandIcon
+              name={targetData.name_kr}
+              size="big"
+            />
+            <div className="brandDescTextDiv">
+              <span className="brandDescName">{targetData.name_kr}</span>
+              <div className="brandMoodDiv">
+                <BrandMoodButton name="퓨어한" />
+                <BrandMoodButton name="자연의" />
+                <BrandMoodButton name="감각적인" />
+              </div>
+            </div>
+          </div>
+          <div className="brandDescText">{targetData.brandDesc}</div>
+        </div>
+      </BrandBannerInsideDiv>
     </BrandBannerWrapper>
   );
 };
@@ -23,10 +44,54 @@ export default BrandBanner;
 const BrandBannerWrapper = styled.div`
   width: 100vw;
   height: 31.35vw;
+  position: relative;
 `;
 
 const BrandBannerDiv = styled.img`
   width: 100%;
   height: 100%;
   object-fit: contain;
+`;
+
+const BrandBannerInsideDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 6.25vw;
+  .brandDescSection {
+    display: flex;
+    flex-direction: column;
+    .brandDescTop {
+      display: flex;
+      flex-direction: row;
+      gap: 1.667vw;
+      .brandDescTextDiv {
+        display: flex;
+        flex-direction: column;
+        gap: 0.938vw;
+      }
+      .brandDescName {
+        font-size: 2.083vw;
+        color: white;
+        font-weight: 700;
+      }
+      .brandMoodDiv {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+      }
+    }
+    .brandDescText {
+      margin-top: 4rem;
+      white-space: pre-wrap;
+      color: white;
+      font-size: 16px;
+      font-weight: 300;
+      line-height: 150%;
+    }
+  }
 `;
--- src/pages/BrandDetail.tsx
+++ src/pages/BrandDetail.tsx
@@ -1,18 +1,18 @@
 import { useParams } from "react-router-dom";
+import imgData from "@/assets/datas/imgData.json";
 
 import BrandBanner from "@/components/BrandBanner";
 
 const BrandDetail = () => {
-
   const { brandId } = useParams<{ brandId: string }>();
+  let targetData = imgData.find((item) => item.id === Number(brandId))!;
 
   return (
     <>
-      <BrandBanner name="이니스프리" />
+      <BrandBanner name={targetData.name_kr} />
       BrandDetail {brandId}
     </>
   );
-
 };
 
 export default BrandDetail;
