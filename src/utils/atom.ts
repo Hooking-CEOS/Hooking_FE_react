85614e80ac909c9e81e2b24b7b190caf44af64dc
hyosin <cdnnnl@gmail.com>
2023-07-19 05:59:01 +0900

    feat: 필터 api params 띄어쓰기 제거

--- src/utils/atom.ts
+++ src/utils/atom.ts
@@ -1,6 +1,6 @@
 import { atom, selectorFamily, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
-import { getCopySearch } from "@/api/copywriting";
+import { getCopySearch, getCopyFilter, getScrapCopy } from "@/api/copywriting";
 
 const { persistAtom } = recoilPersist();
 
@@ -9,6 +9,23 @@ export const checkedFilterList = atom({
   default: [[], [], [], []], // product, age, price, mood
 });
 
+// checkedFilterList를 통해 필터링된 카드 데이터
+export const filterCardList = selector({
+  key: "filterCardList",
+  get: async ({ get }) => {
+    const list = get(checkedFilterList);
+    const key = ["mood", "product", "age", "price"];
+    let params: any = {};
+    list.map((filter: any, idx) => {
+      if (filter.length > 0) {
+        // 콤마로 합친 후 공백 제거
+        // 띄어쓰기 있으면 인식 못함 -> 프리미엄 라인은 되는데 프리미엄라인은 안됨
+        params[key[idx]] = filter.join(",").replace(/ /g, "");
+      }
+    });
+    return await getCopyFilter(params);
+  },
+});
 export const checkedListLen = selector({
   key: "checkedListLen",
   get: ({ get }) => {
@@ -86,4 +103,11 @@ export const activeChildMenu = atom({
 export const isLogined = atom({
   key: "isLogin",
   default: false,
+  effects_UNSTABLE: [persistAtom],
+});
+
+//
+export const isBigWindow = atom({
+  key: "bigWindow",
+  default: false, // 1320 화면 기준, 초깃값은 false
 });
