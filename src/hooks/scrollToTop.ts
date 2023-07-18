37ea9b214d8d6bdab2f904f1e833a14ee364c6c3
hyosin <cdnnnl@gmail.com>
2023-07-18 10:14:42 +0900

    chore: 불필요한 주석 제거

--- src/hooks/scrollToTop.ts
+++ src/hooks/scrollToTop.ts
@@ -3,13 +3,10 @@ import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-  console.log("[pathname]", pathname);
 
   useEffect(() => {
     const app = document.querySelector(".app");
     if (app) {
-      console.log("[app]", app);
-
       app.scrollTop = 0;
     }
   }, [pathname]);
