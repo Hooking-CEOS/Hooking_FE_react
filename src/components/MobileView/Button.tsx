a625efeae2d4205847e015476f0f91c13923d53f
moong23 <moonki0623@naver.com>
2024-02-26 17:56:34 +0900

    feat: Mobile Button에 onClick 메서드 추가

--- src/components/MobileView/Button.tsx
+++ src/components/MobileView/Button.tsx
@@ -4,17 +4,20 @@ interface MobileButtonProps {
   children?: JSX.Element;
   fill?: string;
   borderColor?: string;
+  onClick?: () => void;
 }
 
 const MobileButton = ({
   children,
   fill = "white",
   borderColor,
+  onClick,
 }: MobileButtonProps) => {
   return (
     <MobileButtonWrapper
       fill={fill}
       borderColor={borderColor}
+      onClick={onClick}
     >
       {children}
     </MobileButtonWrapper>
@@ -28,7 +31,7 @@ const MobileButtonWrapper = styled.div<{ fill: string; borderColor?: string }>`
   flex: 0 0 auto;
   gap: 0.6rem;
   border: 1px solid
-    ${(props) => props.borderColor || props.theme.colors.black40};
+    ${(props) => props.borderColor || props.theme.colors.black10};
   background-color: ${(props) => props.fill};
   display: flex;
   justify-content: center;
